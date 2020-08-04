3763863978494425a5bfdade9ca26a0a9d9c5109
SaaD Ibne Jamal <49411865+SaaD036@users.noreply.github.com>
2020-08-04 22:26:11 +0600

    Add files via upload

---
+++ 723A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int x[3];
+
+    for(int i=0; i<3; i++){
+        cin>>x[i];
+    }
+    sort(x, x+3);
+
+    cout<<x[2]-x[0];
+
+    return 0;
+}
